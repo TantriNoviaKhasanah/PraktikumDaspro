10b8c512e9343cd8914100cdb2eedf0025fdb461
TantriNoviaKhasanah <tantrinovia157@gmail.com>
2025-10-08 09:27:58 +0700

    Percobaan 1 Ternary

--- jobsheet05/CetakKRS26.java
+++ jobsheet05/CetakKRS26.java
@@ -2,21 +2,23 @@ import java.util.Scanner;
 
 public class CetakKRS26 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-    if (uktLunas) {
-        System.out.println("Pembayaran UKT terverifikasi");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-    
-    }else{
-        System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
-    }
+        // if (uktLunas) {
+        // System.out.println("Pembayaran UKT terverifikasi");
+        // System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
 
-    }
-    
-    
-    }
+        // } else {
+        // System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih
+        // dahulu");
+        // }
 
+        String hasil = uktLunas
+                ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA"
+                : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+        System.out.println(hasil);
+    }
+}
