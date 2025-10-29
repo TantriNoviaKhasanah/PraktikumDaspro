14bee90bb40b5cf1ba25500c5e7d3d4dbf083724
TantriNoviaKhasanah <tantrinovia157@gmail.com>
2025-10-29 10:03:58 +0700

    Modifikasi Percobaan 1

--- Jobsheet07/siakadFor26.java
+++ Jobsheet07/siakadFor26.java
@@ -5,7 +5,10 @@ public class siakadFor26 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
-        for (int i = 1; i <=10; i++) {
+        int lulus = 0;
+        int tidakLulus = 0;
+
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
             if (nilai > tertinggi) {
@@ -14,8 +17,15 @@ public class siakadFor26 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus :" + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
     }
 }
