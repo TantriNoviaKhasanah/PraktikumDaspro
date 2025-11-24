bb64c119c06b27e073fa6e4bb2a3e66736327f3e
TantriNoviaKhasanah <tantrinovia157@gmail.com>
2025-11-24 12:49:15 +0700

    Percobaan 4

---
+++ Jobsheet10/siakad25.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class siakad25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahSiswa = sc.nextInt();
+
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+
+        int nilai[][] = new int[jumlahSiswa][jumlahMatkul];
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
+            double totalPerSiswa = 0;
+
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Masukkan nilai mata kuliah ke-" + (j + 1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
+            }
+
+            System.out.println("Nilai rata-rata: " + (totalPerSiswa / jumlahMatkul));
+        }
+
+        System.out.println("\n-----------------------------------");
+        System.out.println("Rata-rata nilai setiap mata kuliah");
+
+        for (int j = 0; j < jumlahMatkul; j++) {
+            double totalPerMataKuliah = 0;
+
+            for (int i = 0; i < jumlahSiswa; i++) {
+                totalPerMataKuliah += nilai[i][j];
+            }
+
+            System.out.println("Mata kuliah " + (j + 1) + ": " + (totalPerMataKuliah / jumlahSiswa));
+        }
+    }
+}
