aa84eee52e3fc3004169764352f81adadf9bc515
TantriNoviaKhasanah <tantrinovia157@gmail.com>
2025-12-08 20:24:45 +0700

    Tugas 1

---
+++ Pertemuan14/kubus25.java
@@ -0,0 +1,20 @@
+package Pertemuan14;
+
+public class kubus25 {
+    static int hitungVolume(int sisi) {
+        return sisi * sisi * sisi;
+    }
+
+    static int hitungLuasPermukaan(int sisi) {
+        return 6 * sisi * sisi;
+    }
+
+    public static void main(String[] args) {
+        int sisi = 4;
+        int volume = hitungVolume(sisi);
+        int luas = hitungLuasPermukaan(sisi);
+
+        System.out.println("Volume kubus adalah: " + volume);
+        System.out.println("Luas permukaan kubus adalah: " + luas);
+    }
+}
